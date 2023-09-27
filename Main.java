f53764b2d75f108a04ffc9cf3630b1c41d7b1e19
Luc.Meuse <126908991+Luc-Meuse@users.noreply.github.com>
2023-09-27 10:30:12 -0400

    commit1

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Phone Iphone15ProMax = new Phone();
+        Phone Iphone14 = new Phone();
+        Phone SamsungS23Ultra = new Phone();
+        Phone Pixel7Pro = new Phone();
+        Phone Nokia3310 = new Phone();     
+        Phone Blackberry = new Phone();    
+    }
+}
