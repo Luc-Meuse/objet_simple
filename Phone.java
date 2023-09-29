1df65f76c6dcae84fb1ba73ac9e7e7b0d3204ba6
Luc.Meuse <126908991+Luc-Meuse@users.noreply.github.com>
2023-09-29 10:31:44 -0400

    save

--- Phone.java
+++ Phone.java
@@ -1,9 +1,14 @@
 public class Phone {
 
-    String model;
-    String color;
-
-    public void showInfo(){
-        System.out.println("Phone model " + model + ", color " + color);
+    Phone(){
+        String model;
+        String color;
     }
+
+    Phone(string )    
+    
+    showInfo();
+    }
+    public void showInfo(){
+            System.out.println("Phone model " + model + ", color " + color);
 }
