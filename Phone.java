139a260c514addbcaa3df75765a6533d2d7176bc
Luc Meuse <126908991+Luc-Meuse@users.noreply.github.com>
2023-10-02 17:43:48 +0000

    commit 3

--- Phone.java
+++ Phone.java
@@ -3,16 +3,16 @@ public class Phone {
     String model;
     String color;
 
-    public void showInfo(){
+public void showInfo(){
             System.out.println("Phone model " + model + ", color " + color);
 }
 
-   public Phone(String model, String color) {
+public Phone(String model, String color) {
         this.model = model;
         this.color = color;
-    }
+}
 
- public Phone() {
-        this.model = "Unknown";
-        this.color = "Unknown";
-    }
+public Phone() {
+        this.model = "samsung";
+        this.color = "green;
+}
